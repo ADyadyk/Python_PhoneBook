b94a015c5092bfc5d484cc672711347a03b65c49
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-10-30 16:02:30 +0300

    upgrade program

--- menu.py
+++ menu.py
@@ -8,17 +8,17 @@ def work_with_phonebook():
             print_result(phone_book)
         elif choice == 2:
             last_name = input('Введите фамилию абонента, телефон которого надо найти: ')
-            print(find_by_lastname(phone_book, last_name))
+            find_by_lastname(phone_book, last_name)
         elif choice == 3:
             last_name = input('Введите фамилию абонента, у которого надо изменить телефон: ')
             new_number = input('Введите новый номер этого абонента: ')
-            print(change_number('phonebook.txt', phone_book, last_name, new_number))
+            change_number('phonebook.txt', phone_book, last_name, new_number)
         elif choice == 4:
             last_name = input('Введите фамилию абонента, которого надо удалить из справочника: ')
-            print(delete_by_lastname('phonebook.txt', phone_book, last_name))
+            delete_by_lastname('phonebook.txt', phone_book, last_name)
         elif choice == 5:
             putnumber = input('Введите номер телефона абонента, которого вы хотите узнать: ')
-            print(find_by_number(phone_book, putnumber))
+            find_by_number(phone_book, putnumber)
         elif choice == 6:
             user_data = input('Введите данные нового абонента через запятую (Фамилия,Имя,Телефон,Описание): ')
             add_user('phonebook.txt', phone_book, user_data)
